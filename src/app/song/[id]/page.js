360c344c8e67544d11b76d6ec7b1482b6178f8af
shekhargrace14 <shekhargrace14@gmail.com>
2024-11-19 00:15:15 +0530

    more cha

--- src/app/song/[id]/page.js
+++ src/app/song/[id]/page.js
@@ -6,6 +6,7 @@ import { useContext } from "react";
 import { DataContext } from "@/app/context/DataContext";
 import CircleCard from "@/components/CircleCard";
 import Head from "next/head";
+import Link from "next/link";
 
 const Song = ({ params }) => {
     const { songData } = useContext(DataContext);
@@ -30,7 +31,7 @@ const Song = ({ params }) => {
             <div>
                 <div className="bg-gray-800 rounded">
                     <div className=" lg:container mx-auto  p-2  gap-4 md:flex gap-4 text-white ">
-                        <div className="flex items-center md:w-4/12 h-full rounded overflow-hidden ">
+                        <div className="flex items-center md:w-4/12 h-full rounded overflow-hidden sm:lg-0 mb-4">
                             <Image
                                 src={item.image}
                                 alt={item.title || "Song Image"}
@@ -54,21 +55,25 @@ const Song = ({ params }) => {
                         </div>
                     </div>
                 </div>
-                <main className="lg:flex lg:container mx-auto  p-4 gap-4">
+                <main className="lg:flex lg:container mx-auto  sm:p-4 py-4 gap-4">
                     <section className="lg:w-12/12 w-full">
                         <h2 className="text-xl mb-2 font-semibold">{item.title}</h2>
                         <div
                             className="flex items-center gap-1"
                             dangerouslySetInnerHTML={{ __html: item.content }}
                         />
-                        <div className="border-t-[1px] border-b-[1px] border-gray p-2 my-4 flex justify-end gap-4">
-                            <span className="flex items-center gap-2">
-                                <FaEye /> {item.views}{" "}
-                            </span>
-                            <span className="flex items-center gap-2">
-                                <FaHeart /> {item.likes}{" "}
-                            </span>
+
+                        <div className="border-t-[1px] border-b-[1px] border-gray p-2 my-4 flex justify-between gap-4">
+                            <div className="flex gap-4">
+                                <span className="flex items-center gap-2">
+                                    <FaEye /> {item.views}{" "}
+                                </span>
+                                <span className="flex items-center gap-2">
+                                    <FaHeart /> {item.likes}{" "}
+                                </span>
+                            </div>
                         </div>
+                        <Link href={item.credits} target="_blank"> Credits : {item.creator}</Link>
                         {/* <CircleCard /> */}
                     </section>
                 </main>

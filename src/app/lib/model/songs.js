2b41b867c17a4f455221592198446f76a55a0d0a
shekhargrace14 <shekhargrace14@gmail.com>
2024-12-09 23:09:40 +0530

    content:String

--- src/app/lib/model/songs.js
+++ src/app/lib/model/songs.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 const songModel = new mongoose.Schema({
     title:String,
     image:String,
-    content:Array,
+    content:String,
 })
 export const Song = mongoose.models.songs || mongoose.model("songs",songModel)

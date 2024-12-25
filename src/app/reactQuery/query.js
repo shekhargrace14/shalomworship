4eed3691a7d1c7d955757c3ec32d71eb2ff3a59c
shekhargrace14 <shekhargrace14@gmail.com>
2024-12-25 21:36:34 +0530

    speed optimise

--- src/app/reactQuery/query.js
+++ src/app/reactQuery/query.js
@@ -4,7 +4,7 @@ const url = "https://www.shalomworship.com";
 
 const fetchSongs = async () => {
   // const res = await fetch('https://fakestoreapi.com/products');
-  const res = await fetch( `${url}/api/song/`);
+  const res = await fetch( `${url}/api/song`);
   
   return await res.json();
 };

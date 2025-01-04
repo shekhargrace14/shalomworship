64babc389dcc9b9d18b4204dda8002038738def8
shekhargrace14 <shekhargrace14@gmail.com>
2025-01-04 13:12:23 +0530

    revalidate chages

--- src/app/song/[id]/page.jsx
+++ src/app/song/[id]/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
-export const revalidate = 60 * 60 * 24 * 7;
+export const revalidate = 604800;
+
 export const dynamic = "force-dynamic";
 
 async function fetchSongData(id) {

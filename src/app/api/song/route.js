9dde49c65d5059cedc292997dadedae6907453ae
shekhargrace14 <shekhargrace14@gmail.com>
2025-01-06 08:36:43 +0530

    search deleted

--- src/app/api/song/route.js
+++ src/app/api/song/route.js
@@ -1,8 +1,5 @@
-import { connectionStr } from "@/app/lib/db";
-import { Song } from "@/app/lib/model/songs";
 import prisma from "@/app/lib/prisma";
 
-import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
@@ -10,8 +7,6 @@ export const dynamic = "force-dynamic";
 export async function GET() {
     let data = [];
     try{ 
-        // await mongoose.connect(connectionStr)
-        // data = await prisma.song.findMany()
         data = await prisma.song.findMany({
             include: {
               artist: { // Include the SongArtist relation

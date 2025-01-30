21ea774d37816aa5f33840b774cd34588250efa4
shekhargrace14 <shekhargrace14@gmail.com>
2025-01-30 23:12:10 +0530

    category generateStaticParams

--- src/app/category/[id]/page.jsx
+++ src/app/category/[id]/page.jsx
@@ -1,6 +1,12 @@
-import { fetchArtistBySlug, fetchCategoryBySlug, useGetArtistById } from "@/app/reactQuery/query";
+import { fetchCategory, fetchCategoryBySlug } from "@/app/reactQuery/query";
 import Processor from "@/components/Processor";
-import Image from "next/image";
+
+export async function generateStaticParams() {
+  const categories = await fetchCategory(); // Fetch all songs from your data source
+  return categories.map(category => ({
+    id: category.id.toString(), // Convert to string if necessary
+  }));
+}
 
 const Page = async ({ params }) => {
   const categorySlug = params.id;
@@ -12,32 +18,21 @@ const Page = async ({ params }) => {
 //   console.log(data.song, "categoryData song page data");
   return (
     <>
-      {/* hello {data.name} */}
       <div className="bg-[#000000]  rounded-lg h-[90vh] overflow-y-auto custom-scrollbar">
         <div className="md:flex gap-4 p-4 text-white w-full bg-gradient-to-b from-[#1f1f1f] to-[#000000]">
           <div className=" flex items-center gap-4 w-full ">
-            {/* <div className="h-full w-3/12 sm:mb-0 mb-2 rounded-lg overflow-hidden  bg-[#121212] ">
-              <Image
-                src={data.image || "/default-image.jpg"}
-                alt={data.title || "Song Image"}
-                width={200}
-                height={100}
-                className="bg-gray-800 object-cover h-full w-full"
-                priority={100}
-              />
-            </div> */}
             <div className="sm:w-8/12 grid">
               <h1 className="sm:text-4xl text-4xl font-semibold mb-1 text-white">
-                {data.name}{" "}
+                {data?.name}{" "}
               </h1>
               <p className="text-sm  text-white">Category</p>
             </div>
           </div>
         </div>
-          <h2 className="text-xl font-semibold m-4 text-white">Songs on {data.name}{" "}Category</h2>
+          <h2 className="text-xl font-semibold m-4 text-white">Songs on {data?.name}{" "}Category</h2>
         <section className="w-full">
           <div className="grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-0 ">
-            {data.song.map((item) => (
+            {data?.song.map((item) => (
               <div key={item.songId}>
                 <Processor item={item.songId} />
               </div>

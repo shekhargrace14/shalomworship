20c9be681a570abbf47e4fff0d1d3be8dba19926
Shek@gith7928 <shekhargrace14>
2025-07-08 23:09:48 +0530

    seo fixed

--- src/app/category/[id]/page.tsx
+++ src/app/category/[id]/page.tsx
@@ -14,6 +14,7 @@ export async function generateStaticParams() {
   });
 }
 
+
 export async function generateMetadata({ params }: any) {
   const slugParams = await params.id;
   const category = await fetchCategoryBySlug(slugParams);

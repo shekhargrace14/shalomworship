738a1972b938832ef034dbb10f17acbca6f543b6
Shek@gith7928 <shekhargrace14>
2025-08-11 00:25:00 +0530

    songs added

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -7,10 +7,10 @@ const CategoryCard = ({ category }: any) => {
   return (
     <>
       <Link key={category.id} href={`/category/${category.slug}`}>
-        <div className="category-card h-16 flex justify-between items-end hover:bg-[#1f1f1f] p-2 px-4 py-2 rounded-lg text-shadow-lg/30"   style={{
-    backgroundImage: `linear-gradient(180deg, ${category?.color}, #00000080)`, // fallback to black or any second color
-  }}>
-          <h2 className="text-white text-lg">{category.name}</h2>
+        <div className="category-card h-16 flex justify-between items-end hover:bg-[#1f1f1f] px-2 py-2 rounded-lg text-shadow-lg/30 overflow-hidden" style={{
+          backgroundImage: `linear-gradient(180deg, ${category?.color}, #00000080)`, // fallback to black or any second color
+        }}>
+          <h2 className="text-white text-base">{category.name}</h2>
           {/* <p className=" text-sm text-white" >({category.song.length})</p> */}
 
         </div>

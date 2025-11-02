0a5dd9b07e7f0bca4447676d9d899434230fb613
shekhar <shekhargrace14@gmail.com>
2025-11-02 23:03:33 +0530

    category card changes

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -8,10 +8,10 @@ const CategoryCard = ({ category }: any) => {
     <>
       <Link key={category.id} href={`/category/${category.slug}`}>
         <div className="category-card h-16 flex justify-between items-end hover:bg-[#1f1f1f] px-2 py-2 rounded-lg text-shadow-lg/30 overflow-hidden" style={{
-          backgroundImage: `linear-gradient(180deg, ${category?.color}, #00000080)`, // fallback to black or any second color
+          backgroundImage: `linear-gradient(180deg, ${category?.color}, #00000030)`, // fallback to black or any second color
         }}>
           <h2 className="text-white text-base">{category.title}</h2>
-          <h2 className="text-white text-sm">{category.song.length}</h2>
+          <p className="text-white text-sm">{category.song.length}</p>
 
         </div>
       </Link>

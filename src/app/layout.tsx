af18886225e76c65c8d6f0241eb89fbbafec3e5d
infoshalomworship-pixel <info.shalomworship@gmail.com>
2026-01-22 00:04:10 +0530

    fix(favicon): favicon from public removed and replaced old favicon in /app/

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
 
   icons: {
-    icon: "/favicon.ico",
+    icon: "app/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
